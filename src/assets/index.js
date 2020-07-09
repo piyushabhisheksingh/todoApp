54f3f7d079a26ca9a3536214a24f7b3c76ccf7ee
Piyush Abhishek Singh <piyushabhisheksingh@Piyushs-MacBook-Pro.local>
2020-07-09 23:56:16 +0530

    folder structure completed for the app

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import * as colors from './colors';
+import * as constants from './constants';
+import * as globalStyles from './global.styles';
+import * as labels from './labels';
+
+
+export {
+    colors,
+    constants,
+    globalStyles,
+    labels
+}
--- src/utls/normalize.js
+++ src/utls/normalize.js
@@ -2,7 +2,7 @@ import { Dimensions, PixelRatio } from 'react-native';
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 const scale = SCREEN_WIDTH / 360;
 
-export function normalize(size) {
+export default function normalize(size) {
     /**
     * @param {number} size - size could be padding, width, height, etc..
     */
